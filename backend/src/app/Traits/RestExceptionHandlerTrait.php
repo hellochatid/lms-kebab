a86a3fe3232dbf07f6d915b06f2a95c4fd29ff4e
Hasan <netter23@gmail.com>
2020-02-06 16:47:51 +0700

    Add exception handler

---
+++ backend/src/app/Traits/RestExceptionHandlerTrait.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Traits;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
+trait RestExceptionHandlerTrait
+{
+    use RestExcepcionResponse;
+
+    /**
+     * Creates a new JSON response based on exception type.
+     *
+     * @param Request $request
+     * @param Exception $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function getJsonResponseForException(Request $request, Exception $exception)
+    {
+        if ($exception instanceof NotFoundHttpException || $exception instanceof ModelNotFoundException) {
+            return $this->notFound();
+        }
+
+        if ($exception instanceof UnauthorizedHttpException) {
+            return $this->jsonResponse(401, 'Unauthorized');
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->jsonResponse(405, 'Method Not Allowed');
+        }
+
+        if (!$exception instanceof HttpException) {
+            return $this->jsonResponse(500, 'Internal Server Error');
+        }
+    }
+
+    protected function jsonResponse(int $code, string $message)
+    {
+        $response = [
+            'error' => [
+                'errors' => [
+                    'code' => $code,
+                    'message' => $message,
+                ]
+            ]
+        ];
+        return response()->json($response, $code);
+    }
+}
