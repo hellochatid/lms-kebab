63e5acf5d89c57fa38462acb4aa0d371d22d7a31
Hasan <netter23@gmail.com>
2020-03-02 15:35:45 +0700

    [Frontend] - Create base layout

---
+++ frontend/src/layouts/course.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import "~/assets/css/style.css";
+
+export default {
+  head() {
+    return {
+      link: [
+        {
+          rel: "stylesheet",
+          href:
+            "https://fonts.googleapis.com/css?family=Ubuntu:300i,400,500,700&display=swap"
+        }
+      ]
+    };
+  }
+};
+</script>
