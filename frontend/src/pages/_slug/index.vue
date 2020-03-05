ef2cb05e2473eac207b129f0539c20650faa4fbc
Hasan <netter23@gmail.com>
2020-03-05 10:34:22 +0700

    [Frontend] - Add dynamic routes

---
+++ frontend/src/pages/_slug/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>{{ slug }}</div>
+</template>
+
+<script>
+export default {
+  head() {
+    return this.head;
+  },
+  layout: "main",
+  data() {
+    return {
+      head: {
+        title: "",
+        meta: [
+          {
+            hid: "description",
+            name: "description",
+            content: "llll"
+          }
+        ]
+      },
+      slug: ""
+    };
+  },
+  mounted() {
+    this.head.title = this.$route.params.slug;
+    this.slug = this.$route.params.slug;
+  }
+};
+</script>
