4ff3350fed1bd41a12a06455538e524698d945b5
Oki T <oki.tri@hellochat.id>
2020-03-06 15:34:57 +0700

    [frontend] landing_page_course_list_and_footer

--- frontend/src/layouts/main.vue
+++ frontend/src/layouts/main.vue
@@ -32,7 +32,125 @@
       <nuxt />
     </div>
     <footer>
-      <div class="container">footer</div>
+      <section class="footer_one">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-md-4 col-md-3 col-lg-3">
+              <div class="footer_contact_widget">
+                <h4>CONTACT</h4>
+                <p>329 Queensberry Street, North Melbourne </p>
+                <p>VIC 3051, Australia.</p>
+                <p>123 456 7890</p>
+                <p>support@edumy.com</p>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-md-3 col-lg-2">
+              <div class="footer_company_widget">
+                <h4>COMPANY</h4>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="page-contact.html">Contact</a></li>
+                  <li><a href="#">Become a Teacher</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-md-3 col-lg-2">
+              <div class="footer_program_widget">
+                <h4>PROGRAMS</h4>
+                <ul class="list-unstyled">
+                  <li><a href="#">Nanodegree Plus</a></li>
+                  <li><a href="#">Veterans</a></li>
+                  <li><a href="#">Georgia</a></li>
+                  <li><a href="#">Self-Driving Car</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-md-3 col-lg-2">
+              <div class="footer_support_widget">
+                <h4>SUPPORT</h4>
+                <ul class="list-unstyled">
+                  <li><a href="#">Documentation</a></li>
+                  <li><a href="#">Forums</a></li>
+                  <li><a href="#">Language Packs</a></li>
+                  <li><a href="#">Release Status</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-md-3 col-lg-3">
+              <div class="footer_apps_widget">
+                <h4>MOBILE</h4>
+                <div class="app_grid">
+                  <button class="apple_btn btn-dark">
+                    <span class="icon">
+                      <span class="flaticon-apple"></span>
+                    </span>
+                    <span class="title">App Store</span>
+                    <span class="subtitle">Available now on the</span>
+                  </button>
+                  <button class="play_store_btn btn-dark">
+                    <span class="icon">
+                      <span class="flaticon-google-play"></span>
+                    </span>
+                    <span class="title">Google Play</span>
+                    <span class="subtitle">Get in on</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Our Footer Middle Area -->
+      <section class="footer_middle_area p0">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4 col-md-3 col-lg-3 col-xl-2 pb15 pt15">
+              <div class="logo-widget home1">
+                <img class="img-fluid" src="images/header-logo.png" alt="header-logo.png">
+                <span>EDUMY</span>
+              </div>
+            </div>
+            <div class="col-sm-8 col-md-5 col-lg-6 col-xl-6 pb25 pt25 brdr_left_right">
+              <div class="footer_menu_widget">
+                <ul>
+                  <li class="list-inline-item"><a href="#">Home</a></li>
+                  <li class="list-inline-item"><a href="#">Privacy</a></li>
+                  <li class="list-inline-item"><a href="#">Terms</a></li>
+                  <li class="list-inline-item"><a href="#">Sitemap</a></li>
+                  <li class="list-inline-item"><a href="#">Purchase</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-4 col-lg-3 col-xl-4 pb15 pt15">
+              <div class="footer_social_widget mt15">
+                <ul>
+                  <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                  <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+                  <li class="list-inline-item"><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                  <li class="list-inline-item"><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                  <li class="list-inline-item"><a href="#"><i class="fa fa-google"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Our Footer Bottom Area -->
+      <section class="footer_bottom_area pt25 pb25">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+              <div class="copyright-widget text-center">
+                <p>Copyright Edumy © 2019. All Rights Reserved.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </footer>
   </div>
 </template>
@@ -2471,9 +2589,9 @@ header.header-nav.menu_style_home_seven .ace-responsive-menu > li.last{
   max-width: 495px;
 }
 .home-five .home-text a.btn {
-  border: 2px solid #0a0a0a;
+  border: 2px solid #b66dff;
   border-radius: 27px;
-  color: #0a0a0a;
+  color: #b66dff;
   font-size: 18px;
   margin-top: 15px;
   padding: 10px 50px;
@@ -2483,9 +2601,9 @@ header.header-nav.menu_style_home_seven .ace-responsive-menu > li.last{
   transition: all .4s ease;
 }
 .home-five .home-text a.btn:hover {
-  background-color: #ffffff;
-  border: 2px solid #ffffff;
-  color: #2D174F;
+  background-color: #b66dff;
+  border: 2px solid #b66dff;
+  color: #ffffff;
 }
 .slide.slide-one.home6{
   height: 730px;
@@ -2828,6 +2946,11 @@ header.header-nav.menu_style_home_seven .ace-responsive-menu > li.last{
 }
 .img_hvr_box.home7.four:before {
   background-color: rgba(0, 188, 228, .9);
+}
+.img_hvr_box .material-icons {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  color: #fff;
 }
 .courses_all_btn .btn{
   border: 2px solid;
@@ -10269,7 +10392,7 @@ ul.mc_parent_list ul.mc_child_list:hover .pricing_btn{
 }
 /*== BackGround Images ==*/
 .bg-img2 {
-  background-image: url("https://i.imgur.com/SqAyEqP.png");
+  background-image: url("https://i.imgur.com/9KDtryZ.png");
 }
 .bg-img5 {
   background-attachment: scroll;
@@ -10636,7 +10759,7 @@ section {
   position: relative;
 }
 .divider_home1:before{
-  background-color: rgba(25, 38, 117, 0.8);
+  background-color: rgba(92, 45, 140, 0.8);
   content: "";
   height: 100%;
   left: 0;
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -33,6 +33,9 @@
       <div class="row">
         <div class="col-sm-6 col-lg-3">
           <div class="img_hvr_box" style="background-image: url(https://i.imgur.com/PsYAyVS.png);">
+          <div class="material-icons icon">
+            book
+          </div>
             <div class="overlay">
               <div class="details">
                 <h5>Personalized learning</h5>
@@ -43,6 +46,9 @@
         </div>
         <div class="col-sm-6 col-lg-3">
           <div class="img_hvr_box" style="background-image: url(https://i.imgur.com/H6bxxhQ.png);">
+          <div class="material-icons icon">
+            timer
+          </div>
             <div class="overlay">
               <div class="details">
                 <h5>Learn anywhere, anytime</h5>
@@ -53,6 +59,9 @@
         </div>
         <div class="col-sm-6 col-lg-3">
           <div class="img_hvr_box" style="background-image: url(https://i.imgur.com/2buhu6Y.png);">
+          <div class="material-icons icon">
+            video_library
+          </div>
             <div class="overlay">
               <div class="details">
                 <h5>Tons of materials</h5>
@@ -63,6 +72,9 @@
         </div>
         <div class="col-sm-6 col-lg-3">
           <div class="img_hvr_box" style="background-image: url(https://i.imgur.com/nuiNzNf.png);">
+          <div class="material-icons icon">
+            account_balance_wallet
+          </div>
             <div class="overlay">
               <div class="details">
                 <h5>Affordable price</h5>
@@ -73,6 +85,176 @@
         </div>
       </div>
 
+    </div>
+  </section>
+
+  <section class="divider_home1 parallax bg-img2" data-stellar-background-ratio="0.3">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2 text-center">
+          <div class="divider-one">
+            <p class="color-white">STARTING ONLINE LEARNING</p>
+            <h1 class="color-white text-uppercase">Enhance your skIlls wIth best OnlIne courses</h1>
+            <a class="btn btn-transparent divider-btn" href="#">See plans</a>           
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="top-courses" class="top-courses pb30">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3">
+          <div class="main-title text-center">
+            <h3 class="mt0">Browse Our Top Courses</h3>
+            <p>Best course from our trusted mentors and masters. Tons of learning material for your rapid learning process.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="emply-text-sec">
+            <div class="row" id="masonry_abc">
+              <div class="col-md-6 col-lg-4 col-xl-3 business design">
+                <div class="top_courses">
+                  <div class="thumb">
+                    <img class="img-whp" src="images/courses/t1.jpg" alt="t1.jpg">
+                    <div class="overlay">
+                      <div class="tag">Top course</div>
+                      <div class="icon"><span class="flaticon-like"></span></div>
+                      <a class="tc_preview_course" href="#">Preview Course</a>
+                    </div>
+                  </div>
+                  <div class="details">
+                    <div class="tc_content">
+                      <p>Chef Chenko</p>
+                      <h5>Kebab Master : Starting Business</h5>
+                      <ul class="tc_review">
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                      </ul>
+                    </div>
+                    <div class="tc_footer text-center">
+                      <a href="" class="btn btn-primary btn-sm">Start course</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 col-xl-3 business design">
+                <div class="top_courses">
+                  <div class="thumb">
+                    <img class="img-whp" src="images/courses/t1.jpg" alt="t1.jpg">
+                    <div class="overlay">
+                      <div class="tag">Top course</div>
+                      <div class="icon"><span class="flaticon-like"></span></div>
+                      <a class="tc_preview_course" href="#">Preview Course</a>
+                    </div>
+                  </div>
+                  <div class="details">
+                    <div class="tc_content">
+                      <p>Chef Chenko</p>
+                      <h5>Kebab Master : Starting Business</h5>
+                      <ul class="tc_review">
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                      </ul>
+                    </div>
+                    <div class="tc_footer text-center">
+                      <a href="" class="btn btn-primary btn-sm">Start course</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 col-xl-3 business design">
+                <div class="top_courses">
+                  <div class="thumb">
+                    <img class="img-whp" src="images/courses/t1.jpg" alt="t1.jpg">
+                    <div class="overlay">
+                      <div class="tag">Top course</div>
+                      <div class="icon"><span class="flaticon-like"></span></div>
+                      <a class="tc_preview_course" href="#">Preview Course</a>
+                    </div>
+                  </div>
+                  <div class="details">
+                    <div class="tc_content">
+                      <p>Chef Chenko</p>
+                      <h5>Kebab Master : Starting Business</h5>
+                      <ul class="tc_review">
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                      </ul>
+                    </div>
+                    <div class="tc_footer text-center">
+                      <a href="" class="btn btn-primary btn-sm">Start course</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-lg-4 col-xl-3 business design">
+                <div class="top_courses">
+                  <div class="thumb">
+                    <img class="img-whp" src="images/courses/t1.jpg" alt="t1.jpg">
+                    <div class="overlay">
+                      <div class="tag">Top course</div>
+                      <div class="icon"><span class="flaticon-like"></span></div>
+                      <a class="tc_preview_course" href="#">Preview Course</a>
+                    </div>
+                  </div>
+                  <div class="details">
+                    <div class="tc_content">
+                      <p>Chef Chenko</p>
+                      <h5>Kebab Master : Starting Business</h5>
+                      <ul class="tc_review">
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="material-icons icon">star</i></a></li>
+                      </ul>
+                    </div>
+                    <div class="tc_footer text-center">
+                      <a href="" class="btn btn-primary btn-sm">Start course</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="our-newslatters" class="our-newslatters">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3">
+          <div class="main-title text-center">
+            <h3 class="mt0">Get Newsletter</h3>
+            <p>Get latest news and update from us!</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6 offset-lg-3">          
+          <div class="footer_apps_widget_home1">
+            <form class="form-inline mailchimp_form">
+              <input type="email" class="form-control" placeholder="Email address">
+              <button type="submit" class="btn btn-lg btn-thm dbxshad">Get it now <span class="flaticon-right-arrow-1"></span></button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 
