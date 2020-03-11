3f7d7c0163af2ebc22c3d1dc89564d7ef09858e7
Hasan <netter23@gmail.com>
2020-03-11 09:42:13 +0700

    [Frontend] - Create text editor component

--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -3,11 +3,13 @@ import Sidebar from './Sidebar.vue'
 import ModalConfirmation from './ModalConfirmation.vue'
 import ImageUploader from './ImageUploader.vue'
 import FileUploader from './FileUploader.vue'
+import TextEditor from './TextEditor.vue'
 
 export {
     Navbar,
     Sidebar,
     ModalConfirmation,
     ImageUploader,
-    FileUploader
+    FileUploader,
+    TextEditor
 }
