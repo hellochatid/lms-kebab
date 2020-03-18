f8108da03630105a7e8e2aafa246d3fb1fb2708a
Hasan <netter23@gmail.com>
2020-03-18 23:46:26 +0700

    [Frontend] - member login

--- frontend/src/pages/login.vue
+++ frontend/src/pages/login.vue
@@ -8,8 +8,10 @@
             <div class="breadcrumb_content">
               <h4 class="breadcrumb_title">Logın</h4>
               <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Logın</li>
+                <li class="breadcrumb-item">
+                  <a href="#">Home</a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">Logın</li>
               </ol>
             </div>
           </div>
@@ -23,23 +25,42 @@
         <div class="row">
           <div class="col-sm-12 col-lg-6 offset-lg-3">
             <div class="login_form inner_page">
-              <form action="../course">
+              <form>
                 <div class="heading">
                   <h3 class="text-center">Login to your account</h3>
-                  <p class="text-center">Don't have an account? <a class="text-thm" href="/register">Sign Up!</a></p>
-                </div>
-                 <div class="form-group">
-                    <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email Address">
+                  <p class="text-center">
+                    Don't have an account?
+                    <a class="text-thm" href="/register">Sign Up!</a>
+                  </p>
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
+                  <input
+                    id="email"
+                    class="form-control input"
+                    type="email"
+                    v-model="input.email"
+                    placeholder="Type your email"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    id="password"
+                    class="form-control input"
+                    type="password"
+                    v-model="input.password"
+                    placeholder="Type your password"
+                  />
                 </div>
                 <div class="form-group custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" id="exampleCheck3">
+                  <input type="checkbox" class="custom-control-input" id="exampleCheck3" />
                   <label class="custom-control-label" for="exampleCheck3">Remember me</label>
                   <a class="tdu btn-fpswd float-right" href="#">Forgot Password?</a>
                 </div>
-                <button type="submit" class="btn btn-log btn-block btn-primary">Login</button>
+                <button
+                  type="button"
+                  class="btn btn-log btn-block btn-primary"
+                  @click="postLogin"
+                >Login</button>
               </form>
             </div>
           </div>
@@ -50,6 +71,9 @@
 </template>
 
 <script>
+import auth from "~/services/auth";
+import form from "~/libs/form";
+
 export default {
   head: {
     title: "Login",
@@ -62,34 +86,29 @@ export default {
     ]
   },
   layout: "main",
+  data() {
+    return {
+      input: {
+        email: "",
+        password: ""
+      },
+      disabled: false,
+      formError: false,
+      modal: {
+        hideHeader: true,
+        hideFooter: true
+      }
+    };
+  },
   methods: {
     postLogin(e) {
       if (!form.validation({ email: "required", password: "required" })) return;
 
-      this.$axios
-        .post("/iam/login", {
-          password: this.input.password,
-          email: this.input.email
-        })
-        .then(res => {
-          if (res.status === 200) {
-            if (typeof res.data.success !== "undefined" && !res.data.success) {
-            } else if (res.data.access_token) {
-              var authUser = {
-                name: "John Doe x",
-                displayPicture:
-                  "http://keenthemes.com/preview/metronic/theme/assets/layouts/layout5/img/avatar1.jpg",
-                token: res.data.access_token,
-                authenticated: true
-              };
-              this.$store.commit("users/authUser", authUser);
-              Cookie.set("authUser", authUser);
-              this.$router.push({
-                path: "/course"
-              });
-            }
-          }
+      auth.login(this, "member").then(res => {
+        this.$router.push({
+          path: "/course"
         });
+      });
     }
   }
 };
